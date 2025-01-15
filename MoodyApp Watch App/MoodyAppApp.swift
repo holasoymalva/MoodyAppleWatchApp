959bc3dd382f9f0ddd198f326230e7a0bacd645f
holasoymalva <alquimista3301@gmail.com>
2025-01-14 23:57:46 -0600

    Initial Commit

---
+++ MoodyApp Watch App/MoodyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyAppApp.swift
+//  MoodyApp Watch App
+//
+//  Created by malva on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodyApp Watch AppTests/MoodyApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp_Watch_AppTests.swift
+//  MoodyApp Watch AppTests
+//
+//  Created by malva on 14/01/25.
+//
+
+import Testing
+@testable import MoodyApp_Watch_App
+
+struct MoodyApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
